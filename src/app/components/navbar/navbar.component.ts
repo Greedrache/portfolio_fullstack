b66d5582ea888795c69ec2a8efbfe65ace7248e0
Greedrache <greedrache@gmail.com>
2026-04-08 10:22:00 +0200

    light and dark mode works

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,12 +9,39 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   toggleMenu() {
-    const nav = document.getElementById("nav");
-    if (nav) {
-      nav.classList.toggle("active");
+    if (isPlatformBrowser(this.platformId)) {
+      const nav = document.getElementById("nav");
+      if (nav) {
+        nav.classList.toggle("active");
+      }
     }
   }
 
+  toggleTheme() {
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.toggle('light-theme');
+
+      if (document.body.classList.contains('light-theme')) {
+        localStorage.setItem('theme', 'light');
+      } else {
+        localStorage.setItem('theme', 'dark');
+      }
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme === 'light') {
+        document.body.classList.add('light-theme');
+      } else {
+        document.body.classList.remove('light-theme');
+      }
+    }
+  }
 }
